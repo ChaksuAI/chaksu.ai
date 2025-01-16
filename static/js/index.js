ca5aaef1d9b32109978019817449c627440c3108
Sasi Alavala <spectrumlabiisc@gmail.com>
2025-01-16 18:29:28 +0530

    new

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,61 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
+  // Initialize Image Carousel
+  var imageCarouselOptions = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    threshold: 10
+  };
+  var imageCarousel = bulmaCarousel.attach('#image-carousel', imageCarouselOptions);
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
+  // Initialize Video Carousel
+  var videoCarouselOptions = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: false,
+    pauseOnHover: false,
+    threshold: 10
+  };
+  var videoCarousel = bulmaCarousel.attach('#video-carousel', videoCarouselOptions);
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
+  // Hide non-active items after each transition
+  function hideNonActiveItems(carouselInstance) {
+    carouselInstance.items().each(function(index) {
+      if (index !== carouselInstance.index()) {
+        $(this).css({
+          'visibility': 'hidden',
+          'z-index': '0',
+          'width': '0'
+        });
+      } else {
+        $(this).css({
+          'visibility': 'visible',
+          'z-index': '1',
+          'width': '100%'
+        });
+      }
+    });
+  }
 
-})
+  imageCarousel.on('afterMove.carousel', function(instance) {
+    hideNonActiveItems(instance);
+    $(window).trigger('resize'); // Force layout recalculation
+  });
+
+  videoCarousel.on('afterMove.carousel', function(instance) {
+    hideNonActiveItems(instance);
+    $(window).trigger('resize'); // Force layout recalculation
+  });
+
+  // Initial hiding of non-active items
+  hideNonActiveItems(imageCarousel);
+  hideNonActiveItems(videoCarousel);
+
+  bulmaSlider.attach();
+});
